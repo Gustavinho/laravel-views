f8ea7e7bea325f757db2a0bd130e2b78499280f5
Eugene van der Merwe <eugene@vander.host>
2021-01-02 14:19:10 +0200

    Update table.blade.php to fix feather icons disappearing
    See https://github.com/livewire/livewire/issues/1083

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -24,8 +24,12 @@ props:
                   {{ $header->title }}
                 </a>
                 <a href="#!" wire:click.prevent="sort('{{ $header->sortBy }}')" class="flex">
-                  <i data-feather="chevron-up" class="{{ $sortBy === $header->sortBy && $sortOrder === 'asc' ? 'text-gray-900' : 'text-gray-400'}} h-4 w-4"></i>
-                  <i data-feather="chevron-down" class="{{ $sortBy === $header->sortBy && $sortOrder === 'desc' ? 'text-gray-900' : 'text-gray-400'}} h-4 w-4"></i>
+                  <span wire:ignore>
+                    <i data-feather="chevron-up" class="{{ $sortBy === $header->sortBy && $sortOrder === 'asc' ? 'text-gray-900' : 'text-gray-400'}} h-4 w-4"></i>
+                  </span>
+                  <span wire:ignore>
+                    <i data-feather="chevron-down" class="{{ $sortBy === $header->sortBy && $sortOrder === 'desc' ? 'text-gray-900' : 'text-gray-400'}} h-4 w-4"></i>
+                  </span>
                 </a>
               </div>
             @else
