1b3fdd9b5b9f88d6565948aa0231c8e2616f9315
Gustavo Martinez <gustavinho.martinez@gmail.com>
2021-02-01 15:09:44 -0600

    Fix typo

--- resources/views/components/actions.blade.php
+++ resources/views/components/actions.blade.php
@@ -10,7 +10,7 @@
 
       @foreach ($actions as $action)
         @if ($action->renderIf($item))
-          <a href="#!" wire:click.prevent="executeAction('{{ $action->id }}', '{{ $item->id }}', true)" class="group flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+          <a href="#!" wire:click.prevent="executeAction('{{ $action->id }}', '{{ $item->id }}', true)" title="{{ $action->title}}" class="group flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 hover:text-gray-900">
             <i data-feather="{{ $action->icon }}" class="mr-3 h-4 w-4 text-gray-600 group-hover:text-gray-700"></i>
             {{ $action->title }}
           </a>
