9403e71802536bd61650dfb070c7c893956d2f31
Muhammad Imamul Azmi <imamul.azmi48@gmail.com>
2021-04-05 01:33:52 +0800

    using model primary key function for execute Actions parameter instead hardcoded with id column

--- resources/views/components/actions.blade.php
+++ resources/views/components/actions.blade.php
@@ -11,7 +11,7 @@
 
         @foreach ($actions as $action)
           @if ($action->renderIf($model))
-            <a href="#!" wire:click.prevent="executeAction('{{ $action->id }}', '{{ $model->id }}', true)" title="{{ $action->title}}" class="group flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+            <a href="#!" wire:click.prevent="executeAction('{{ $action->id }}', '{{ $model->getKey() }}', true)" title="{{ $action->title}}" class="group flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 hover:text-gray-900">
               <i data-feather="{{ $action->icon }}" class="mr-3 h-4 w-4 text-gray-600 group-hover:text-gray-700"></i>
               {{ $action->title }}
             </a>
@@ -24,7 +24,7 @@
     <div class="hidden lg:flex justify-items-end">
       @foreach ($actions as $action)
         @if ($action->renderIf($model))
-          <x-lv-icon-button :icon="$action->icon" size="sm" wire:click.prevent="executeAction('{{ $action->id }}', '{{ $model->id }}', true)" />
+          <x-lv-icon-button :icon="$action->icon" size="sm" wire:click.prevent="executeAction('{{ $action->id }}', '{{ $model->getKey() }}', true)" />
         @endif
       @endforeach
     </div>
