20f146f60a499cb7bf98dc9e7de728b200a54bb9
cole.shirley <cole.shirley@flexsim.com>
2021-05-27 12:04:28 -0600

    updated select button with icon prop

--- resources/views/components/buttons/select.blade.php
+++ resources/views/components/buttons/select.blade.php
@@ -8,13 +8,14 @@ it could be primary, primary-light
 You can customize the variants classes in config/laravel-views.php
 
 props
- - title 
- - block
- - variant --}}
+ - title
+ - icon --}}
+@props(['icon' => 'chevron-down'])
 <button
   class="w-full bg-white border border-gray-300 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none text-left">
   <div class="truncate overflow-x-hidden">{{ $title }}</div>
   <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-    <i data-feather="chevron-down" class="w-4 h-4"></i>
+    <i data-feather="{{ $icon }}"
+      class="w-4 h-4"></i>
   </div>
 </button>
