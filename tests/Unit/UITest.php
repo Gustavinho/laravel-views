693925f3c04578236684ea5b1e8f82fbdd077da9
Gustavo Martinez <gustavinho.martinez@gmail.com>
2021-05-30 16:26:47 -0500

    Changed UI test assertions

--- tests/Unit/UITest.php
+++ tests/Unit/UITest.php
@@ -2,6 +2,7 @@
 
 namespace LaravelViews\Test\Unit;
 
+use DOMDocument;
 use LaravelViews\Facades\UI;
 use LaravelViews\Test\TestCase;
 
@@ -10,45 +11,62 @@ class UITest extends TestCase
     public function testBagdeDefaultHelper()
     {
         $badge = UI::badge('active');
-        $this->blade($badge)
-            ->assertSeeText('active');
+        $expected = '<span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full '.variants('badges.default').'">
+                        active
+                    </span>';
+
+        $this->assertHtmlEquals($badge, $expected);
     }
 
     public function testBagdeSuccesstHelper()
     {
         $badge = UI::badge('active', 'success');
-        $this->blade($badge)
-            ->assertSeeText('active')
-            ->assertSee(variants('badges.success'));
+        $expected = '<span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full '.variants('badges.success').'">
+                        active
+                    </span>';
+
+        $this->assertHtmlEquals($badge, $expected);
     }
 
     public function testAvatarHelper()
     {
         $avatar = UI::avatar('my-avatar-url');
-        $this->blade($avatar)
-            ->assertSee(variants('images.avatar'));
+        $expected = '<img src="my-avatar-url" alt="my-avatar-url" class="'.variants('images.avatar').'">';
+
+        $this->assertHtmlEquals($avatar, $expected);
     }
 
     public function testLinkHelper()
     {
         $link = UI::link('title', '/');
-        $this->blade($link)
-            ->assertSee(variants('links.default'))
-            ->assertSeeText('title');
+        $expected = '<a href="/" class="'.(variants('links.default')).'">
+                        title
+                    </a>';
+
+        $this->assertHtmlEquals($link, $expected);
     }
 
     public function testDefaultIconHelper()
     {
         $icon = UI::icon('activity');
-        $this->blade($icon)
-            ->assertSee('activity');
+        $expected = '<i data-feather="activity" class=" "></i>';
+
+        $this->assertHtmlEquals($icon, $expected);
     }
 
     public function testIconHelperWithVariant()
     {
         $icon = UI::icon('activity', 'success');
-        $this->blade($icon)
-            ->assertSee(variants('icons.success'))
-            ->assertSee('activity');
+        $expected = '<i data-feather="activity" class="'.variants('icons.success').' "></i>';
+
+        $this->assertHtmlEquals($icon, $expected);
+    }
+
+    private function assertHtmlEquals($html, $expected)
+    {
+        $this->assertEquals(
+            preg_replace('/^\s+|\n|\r|\s+$/m', '', $html),
+            preg_replace('/^\s+|\n|\r|\s+$/m', '', $expected)
+        );
     }
 }
