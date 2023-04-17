58e85479f239b4acc03b780752d55eeca069e47a
Razziel06 <ricardo.m.ferreira@live.com.pt>
2023-04-17 10:15:25 +0100

    Added sortNatural header to sort() method on wire:click event

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -27,10 +27,10 @@ props:
           @else
             @if ($header->isSortable())
               <div class="flex">
-                <a href="#!" wire:click.prevent="sort('{{ $header->sortBy }}')" class="flex-1">
+                <a href="#!" wire:click.prevent="sort('{{ $header->sortBy }}', {{ $header->sortNatural }})" class="flex-1">
                   {{ $header->title }}
                 </a>
-                <a href="#!" wire:click.prevent="sort('{{ $header->sortBy }}')" class="flex">
+                <a href="#!" wire:click.prevent="sort('{{ $header->sortBy }}', {{ $header->sortNatural }})" class="flex">
                   <i data-feather="chevron-up" class="{{ $sortBy === $header->sortBy && $sortOrder === 'asc' ? 'text-gray-900' : 'text-gray-400'}} h-4 w-4"></i>
                   <i data-feather="chevron-down" class="{{ $sortBy === $header->sortBy && $sortOrder === 'desc' ? 'text-gray-900' : 'text-gray-400'}} h-4 w-4"></i>
                 </a>
